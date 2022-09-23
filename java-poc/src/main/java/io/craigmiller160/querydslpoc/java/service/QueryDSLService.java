a5c1717a9eb228a62be0db82b43be01f16e155b4
Craig Miller <craigmiller160@gmail.com>
2022-09-23 12:59:35 -0400

    Got everything to compile

--- java-poc/src/main/java/io/craigmiller160/querydslpoc/java/service/QueryDSLService.java
+++ java-poc/src/main/java/io/craigmiller160/querydslpoc/java/service/QueryDSLService.java
@@ -2,6 +2,7 @@ package io.craigmiller160.querydslpoc.java.service;
 
 import com.querydsl.jpa.JPQLQueryFactory;
 import io.craigmiller160.querydslpoc.java.data.entity.Employee;
+import io.craigmiller160.querydslpoc.java.data.entity.QEmployee;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,10 @@ public class QueryDSLService {
     private final JPQLQueryFactory queryFactory;
 
     public Optional<Employee> getOneEmployee() {
-        final var nullableResult = queryFactory.query()
+        final Employee nullableResult = queryFactory.query()
                 .from(QEmployee.employee)
                 .limit(1)
+                .select(QEmployee.employee)
                 .fetchFirst();
         return Optional.ofNullable(nullableResult);
     }
