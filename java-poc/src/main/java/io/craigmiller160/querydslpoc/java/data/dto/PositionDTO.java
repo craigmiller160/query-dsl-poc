f08c168325873328b84e7a35ca46de666e4aa014
Craig Miller <craigmiller160@gmail.com>
2022-09-23 12:29:16 -0400

    Adding mapper

---
+++ java-poc/src/main/java/io/craigmiller160/querydslpoc/java/data/dto/PositionDTO.java
@@ -0,0 +1,11 @@
+package io.craigmiller160.querydslpoc.java.data.dto;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class PositionDTO {
+    private UUID id;
+    private String name;
+}
---
+++ java-poc/src/main/java/io/craigmiller160/querydslpoc/java/data/mapper/PositionMapper.java
@@ -0,0 +1,10 @@
+package io.craigmiller160.querydslpoc.java.data.mapper;
+
+import io.craigmiller160.querydslpoc.java.data.dto.PositionDTO;
+import io.craigmiller160.querydslpoc.java.data.entity.Position;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PositionMapper {
+    PositionDTO positionToDTO(final Position position);
+}
