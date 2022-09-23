8b45bb5b9df054e13af8be09700c19da354e6e4f
Craig Miller <craigmiller160@gmail.com>
2022-09-23 12:55:23 -0400

    Added another test

---
+++ java-poc/src/test/java/io/craigmiller160/querydslpoc/java/QueryDSLTest.java
@@ -0,0 +1,24 @@
+package io.craigmiller160.querydslpoc.java;
+
+import io.craigmiller160.querydslpoc.java.service.QueryDSLService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
+public class QueryDSLTest {
+    private final QueryDSLService queryDSLService;
+
+    @Test
+    public void test_queryDslOperation() {
+        final var result = queryDSLService.getOneEmployee();
+        assertThat(result).isPresent();
+    }
+}
