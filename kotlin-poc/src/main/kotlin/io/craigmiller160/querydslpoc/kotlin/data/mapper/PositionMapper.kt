e05ba2903b33f745450262c38d57ed845ad16045
Craig Miller <craigmiller160@gmail.com>
2022-09-23 12:37:29 -0400

    Added dto and mapper

---
+++ kotlin-poc/src/main/kotlin/io/craigmiller160/querydslpoc/kotlin/data/mapper/PositionMapper.kt
@@ -0,0 +1,10 @@
+package io.craigmiller160.querydslpoc.kotlin.data.mapper
+
+import io.craigmiller160.querydslpoc.kotlin.data.dto.PositionDTO
+import io.craigmiller160.querydslpoc.kotlin.data.entity.Position
+import org.mapstruct.Mapper
+
+@Mapper(componentModel = "spring")
+interface PositionMapper {
+    fun positionToDTO(position: Position): PositionDTO
+}
