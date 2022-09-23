649e1f6c5344bafe60c366211d81e9c438ecf40f
Craig Miller <craigmiller160@gmail.com>
2022-09-23 11:56:00 -0400

    Setting up db stuff

---
+++ java-poc/src/main/java/io/craigmiller160/querydslpoc/java/Runner.java
@@ -0,0 +1,11 @@
+package io.craigmiller160.querydslpoc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runner {
+    public static void main(final String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+}
--- java-poc/src/test/java/io/craigmiller160/querydslpoc/java/LombokAndJpaTest.java
+++ java-poc/src/test/java/io/craigmiller160/querydslpoc/java/LombokAndJpaTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
